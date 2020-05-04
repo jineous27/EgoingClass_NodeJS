49f7f32f0b1a4b425a2920f67bc40dccc9abc50e
jinheeyi <jinhee.yi27@gmail.com>
2020-05-05 06:34:02 +1000

    콘솔에서 입력값 / No found / 반복문

---
+++ syntax/loop.js
@@ -0,0 +1,11 @@
+console.log('A');
+console.log('B');
+
+//반복문에서 몇번반복할 것인가가 중요하다. 
+var i= 0; // 반복문이 호출될 때마다, 0부터 시작한다. 
+while(i < 2){
+    console.log('C1');
+    console.log('C2');
+    i = i + 1;
+}
+console.log('D');
