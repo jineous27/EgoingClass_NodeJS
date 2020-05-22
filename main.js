719f6b62ed68e3cfb0c20f509b65456cfc2ba584
jinheeyi <jinhee.yi27@gmail.com>
2020-05-23 05:38:34 +1000

    App-글수정 - 수정할 저보 전송

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ var app = http.createServer(function(request,response){
         var title = 'WEB - create';
         var list = templateList(filelist);
         var template = templateHTML(title, list, `
-          <form action="http://localhost:3000/create_process" method="post">
+          <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="제목"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -75,7 +75,7 @@ var app = http.createServer(function(request,response){
             <p>
               <input type="submit">
             </p>
-          </form>
+          </form> 
         `, '');
         response.writeHead(200);
         response.end(template);
@@ -95,9 +95,30 @@ var app = http.createServer(function(request,response){
           })
       });
 
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
+    } else if (pathname === '/update'){
+      fs.readdir('./data', function(error, filelist){
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, 
+            `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="제목" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+          </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">Update</a>`
+            );
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
     }
  
  
