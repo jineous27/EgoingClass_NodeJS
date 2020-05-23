5dacf6bbe817dc585fcdd711819a5402ec5ee84f
jinheeyi <jinhee.yi27@gmail.com>
2020-05-24 07:16:21 +1000

    객체와 반복문을 같이 사용해보자-!
    객체화 된 정보들을 반복문을 사용해서 작성해 보자-!

--- syntax/object.js
+++ syntax/object.js
@@ -1,11 +1,38 @@
-var member = ['egoing', 'k8805', 'James']; //배열안에 정보가 담긴 것 Array의 예시'//
-console.log(member[1]);
+var members = ['egoing', 'k8805', 'James']; //배열안에 정보가 담긴 것 Array의 예시'//
+console.log(members[1]);
 
-//역할에 대한 정보를 담을 때 더 적합하다. 표현방법은 { } 중괄호를 씀//
-var role = { 
-    'programmer' : 'egoing',
+var i = 0;
+while(i < members.length){
+    console.log('array loop', members[i]);
+    i = i + 1;
+}
+
+var roles = {
+    'programmer':'egoing',
     'designer' : 'k8805',
     'manager' : 'hoya'
-}   
-console.log(role.manager); 
+}
+console.log(roles.designer); //k8805
+console.log(roles['designer']);
+
+for(var n in roles){
+    console.log('object => ', n, 'value =>', roles[n]);
+}
+
+
+
+
+
+
+
+
+
+//역할에 대한 정보를 담을 때 더 적합하다. 표현방법은 { } 중괄호를 씀//
+// var role = { 
+//     'programmer' : 'egoing',
+//     'designer' : 'k8805',
+//     'manager' : 'hoya'
+// }   
+// console.log(role.manager); 
 //Array 에서는 [] 를 썻지만, 객체 (Object)에서는 .'쩜'을 찍어서 '이름'을 표시해 준다'//
+
